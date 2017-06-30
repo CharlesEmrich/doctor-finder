3d21fe520c3a42db30d4bad654ce33c8614a2212
Charles Emrich <libertinistic@gmail.com>
2017-06-30 11:53:38 -0700

    add sample code

--- js/complaint-interface.js
+++ js/complaint-interface.js
@@ -3,6 +3,6 @@ var Complaint = require('./../js/complaint.js').complaintModule;
 
 $(document).ready(function() {
   var newComplaint = new Complaint();
-
-  console.log(newComplaint.getConditions());
+  // newComplaint.getConditions();
+  newComplaint.starterCode($('input[name="complaint"]').val());
 });
